f6c9bad5499bb985478d7f1682b35a99b0e86643
jackkdev <j4ckk3@gmail.com>
2025-10-30 19:17:00 -0500

    feat(rca): added rca implementation

--- src/rca.sv
+++ src/rca.sv
@@ -7,4 +7,16 @@ module rca #(
   output logic [N - 1:0] y,
   output logic c_out
 );
+  logic [N:0] c; // Stores the N different carry signals. 
+  assign c[0] = c_in; // Set the first carry signal as our carry in.
+  assign c_out = c[N]; // Set the carry out as the last carry signal.
+  
+  // Generate the different units.
+  genvar i;
+  for (i = 0; i < N; i++) begin : blocks
+    logic p;
+    assign p = a[i] ^ b[i]; // The "propogate" signal.
+    assign y[i] = p ^ c[i]; // The output bit.
+    assign c[i + 1] = (p & c[i]) | (a[i] & b[i]); // The carry bit.
+  end
 endmodule
